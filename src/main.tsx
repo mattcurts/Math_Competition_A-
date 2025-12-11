d81c8a05df7b146844886449576f40e8b12ac977
Matthew Curtis <matthewcurtis@uvic.ca>
2025-12-11 12:39:44 -0800

    fix: use the right library for speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from "react-dom/client";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
